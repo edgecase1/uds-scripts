68ac772b732e10ee2fb95817588f30b0ed39fd0b
rnd <rkugler@sba-research.org>
2025-12-25 11:12:29 +0000

    restart script

---
+++ peakcan_restart_can0.sh
@@ -0,0 +1,13 @@
+
+perror()
+{
+	echo $* >&2
+	exit 1
+}
+
+ip link show dev can0 >/dev/null || perror "interface can0 not found"
+
+ip link set down dev can0
+ip link set up dev can0 type can bitrate 500000
+
+ip link show dev can0
